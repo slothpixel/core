76c78624ef953a8f2fb858dd1eec417a881bcf60
builder-247 <olaakkonen@gmail.com>
2018-09-14 16:18:59 +0300

    Added testing

---
+++ test/test.js
@@ -0,0 +1,32 @@
+/* global describe it */
+/**
+ * Main test script to run tests
+ * */
+process.env.NODE_ENV = 'test';
+const nock = require('nock');
+const assert = require('assert');
+// const supertest = require('supertest');
+
+const buildPlayer = require('../store/buildPlayer');
+
+const playerApi = require('./data/player');
+const guildApi = require('./data/guild');
+
+// fake api responses
+nock('https://api.hypixel.net/')
+  // fake player stats
+  .get('/player')
+  .query(true)
+  .reply(200, playerApi)
+  // fake guild stats
+  .get('/guild')
+  .query(true)
+  .reply(200, guildApi);
+
+describe('parseStats', () => {
+  it('should return parsed player stats', () => {
+    buildPlayer('ef962ec2df6e48a2ac9d6062c1b84652', (err, player) => {
+      assert.equal(player.uuid, 'ef962ec2df6e48a2ac9d6062c1b84652');
+    });
+  });
+});
