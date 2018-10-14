1d58118d6779ca76b53fcf7674646c432101217f
mattco98 <matthewcolsson@gmail.com>
2018-10-13 19:13:18 -0500

    Started guild medal parser
    Implemented function to grab guild medals. The code could probably be cleaned up, and it requires further testing. There are most likely edge cases that will not be picked up as is. Part of #23.

--- store/buildGuild.js
+++ store/buildGuild.js
@@ -1,9 +1,65 @@
 /* eslint-disable consistent-return */
+const cheerio = require('cheerio');
+const cloudscraper = require('cloudscraper');
+const { game_types: gameTypes } = require('hypixelconstants');
 const config = require('../config');
 const processGuildData = require('../processors/processGuildData');
 const utility = require('../util/utility');
 const redis = require('../store/redis');
 const queries = require('../store/queries');
+
+/**
+ * Scrapes the guild medals from the Hypixel forums
+ * for the specified guild
+ */
+function getGuildMedals(id, cb) {
+  const medals = {
+    general: {},
+    games: {},
+  };
+
+  cloudscraper.get(`https://hypixel.net/guilds/${id}`, (err, _, body) => {
+    if (err) return cb(err);
+
+    const $ = cheerio.load(body);
+
+    $('.guild-medal').each(function() {
+      const elem = $(this);
+      const text = elem.parent().last().text();
+
+      if (elem.hasClass('overall')) {
+        const level = /Guild Level (\d+)/g.exec(text);
+        const rank = /Guild Rank (\d+)/g.exec(text);
+
+        medals.general.level = (level && level[1]) || 0;
+        medals.general.rank = (rank && rank[1]) || 0;
+      } else if (elem.hasClass('legacy-ranking')) {
+        const legacy = /Legacy Rank (\d+)/g.exec(text);
+        medals.general.legacy = (legacy && legacy[1]) || 0;
+      } else {
+        for (let i = 0; i < gameTypes.length; i += 1) {
+          const game = gameTypes[i];
+
+          // Search for any of the four game names in the 
+          // html element text
+          const names = Object.values(game)
+            .filter(v => typeof v === 'string')
+            .join('|');
+
+          const re = new RegExp(`^(?:${names}) .+ (\\d+)`, 'i');
+          const match = re.exec(text.replace('The', '').trim());
+
+          if (match) {
+            medals.games[game.standard_name] = match[1]; // eslint-disable-line
+            break;
+          }
+        }
+      }
+    });
+
+    cb(null, medals);
+  });
+}
 
 /*
 * Functions to build/cache guild object
@@ -14,12 +70,16 @@ function getGuildData(id, cb) {
     id,
   });
   utility.getData(url, (err, body) => {
-    if (err) {
-      return cb(err);
-    }
-    // TODO - Add medal HTML parser
-    const guild = processGuildData(body.guild);
-    return cb(null, guild);
+    if (err) return cb(err);
+
+    getGuildMedals(id, (err, medals) => {
+      if (err) return cb(err);
+
+      cb(null, {
+        ...processGuildData(body.guild),
+        medals,
+      });
+    });
   });
 }
 
@@ -28,9 +88,7 @@ function getGuildID(uuid, cb) {
     id: uuid,
   });
   utility.getData(url, (err, foundguild) => {
-    if (err) {
-      return cb(err);
-    }
+    if (err) return cb(err);
     return cb(null, foundguild.guild);
   });
 }
@@ -43,6 +101,7 @@ function cacheGuild(guild, id, key, cb) {
       }
     });
   }
+
   if (config.ENABLE_DB_CACHE) {
     queries.insertGuild(id, guild, (err) => {
       if (err) {
@@ -50,7 +109,8 @@ function cacheGuild(guild, id, key, cb) {
       }
     });
   }
-  return cb(guild);
+  
+  cb(guild);
 }
 
 function buildGuild(uuid, cb) {
@@ -66,14 +126,14 @@ function buildGuild(uuid, cb) {
       if (err) {
         return cb(err);
       }
+
       if (reply) {
         const guild = JSON.parse(reply);
         return cb(null, guild);
       }
+
       getGuildData(id, (err, guild) => {
-        if (err) {
-          return cb(err);
-        }
+        if (err) return cb(err);
         cacheGuild(guild, id, key, guild => cb(null, guild));
       });
     });
