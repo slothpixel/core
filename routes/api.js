6f91e05ef9a820274c1884a8989fec4c92cae493
builder-247 <olaakkonen@gmail.com>
2018-10-30 15:42:00 +0200

    "/" route now returns api spec

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,12 @@
 const express = require('express');
-
 const spec = require('./spec');
 
 const api = new express.Router();
+
+// API spec
+api.get('/', (req, res) => {
+  res.json(spec);
+});
 
 // API endpoints
 Object.keys(spec.paths).forEach((path) => {
