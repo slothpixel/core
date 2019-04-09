19be7fe000c5fb1c714b868b3d45f543a9313ba2
builder_247 <olaakkonen@gmail.com>
2019-04-09 16:44:10 +0300

    Fixed builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,4 @@ set -e
 
 sudo docker run -d --privileged --name redis --net=host redis:5
 sudo docker run -d --privileged --name mongo --net=host mongo
-sudo docker build -t "slothpixel/core" . && sudo docker run -i slothpixel/core sh -c 'npm run test'
+sudo docker build -t "slothpixel/core" . && sudo docker run --net=host -i slothpixel/core sh -c 'npm run test'
