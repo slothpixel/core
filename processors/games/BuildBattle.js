01bacfef9f8d85c98374308a781980af3aa32d43
builder_247 <olaakkonen@gmail.com>
2019-06-24 15:38:30 +0300

    Build Battle stats #20

--- processors/games/BuildBattle.js
+++ processors/games/BuildBattle.js
@@ -1,8 +1,34 @@
+/* eslint-disable camelcase */
+const { getRatio } = require('../../util/utility');
 /*
 * Build Battle
+* TODO - Theme vote stats
  */
 module.exports = ({
   coins = 0,
+  score = 0,
+  wins = 0,
+  total_votes = 0,
+  wins_solo_normal = 0,
+  wins_solo_pro = 0,
+  wins_teams_normal = 0,
+  wins_guess_the_build = 0,
+  correct_guesses = 0,
+  games_played = 0,
+  super_votes = 0,
+  packages = [],
 }) => ({
   coins,
+  score,
+  wins,
+  w_r: getRatio(wins, (games_played - wins)),
+  total_votes,
+  wins_solo_normal,
+  wins_solo_pro,
+  wins_teams_normal,
+  wins_guess_the_build,
+  correct_guesses,
+  games_played,
+  super_votes,
+  packages,
 });
