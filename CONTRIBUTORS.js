9edeaa6013e71fbbecaaaf9e119f5796840182f8
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2019-10-29 16:28:07 -0700

    Updated Contributors and README

--- CONTRIBUTORS.js
+++ CONTRIBUTORS.js
@@ -1,10 +1,10 @@
 /*
-* This file is for contributors to Slothpixel to add their minecraft uuid.
+* This file is for contributors to Slothpixel to add their Minecraft UUID.
 * Currently this will cause the /player/:id endpoint to return is_contributor = true for your profile
 */
 
 module.exports = {
-  ef962ec2df6e48a2ac9d6062c1b84652: {}, // builder_247
+  'ef962ec2df6e48a2ac9d6062c1b84652': {}, // builder_247
   '7beb3348294f44d8b967cdf0d872aff8': {}, // ecolsson
   '63fd5c413cd641e987d473df9c0e1e56': {}, // ThePoptartCrpr
   '532de453ac214ddeaec9a18a24a79e27': {}, // Cham
