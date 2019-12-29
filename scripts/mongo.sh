b7a32b903fa8800cbeace490900c0464a1653e8c
builder_247 <olaakkonen@gmail.com>
2019-12-29 23:15:18 +0200

    fix

--- scripts/mongo.sh
+++ scripts/mongo.sh
@@ -5,5 +5,5 @@ curl -sSL https://get.docker.com/ | sh
 sudo mkdir -p /var/lib/mongodb
 sudo mount -o discard,defaults /dev/disk/by-id/google-persistent-disk-1 /var/lib/mongodb
 
-sudo docker run -d --name mongo --restart=always --log-opt max-size=1g -d --net=host -v /var/lib/mongodb:/var/lib/mongodb -e mongo
+sudo docker run -d --name mongo --restart=always --log-opt max-size=1g -d --net=host -v /var/lib/mongodb:/var/lib/mongodb mongo
 sudo docker start mongo
