5258da0032a5b60bcf9ccbe23c8c114109b88b4e
builder_247 <olaakkonen@gmail.com>
2020-03-19 21:22:05 +0200

    Total kit wins

--- processors/games/HungerGames.js
+++ processors/games/HungerGames.js
@@ -150,6 +150,7 @@ module.exports = ({
     wins: {
       solo: getKitStat(/^wins_(?!teams_)/),
       teams: getKitStat(/^wins_teams_/),
+      total: {},
     },
     kills: getKitStat(/^kills_/),
     k_d: {},
@@ -178,8 +179,9 @@ module.exports = ({
     },
   };
 
-  // Calculate k_d and w_l for kits
+  // Calculate k_d, w_l and total wins for kits
   Object.keys(kit_stats.games_played).forEach((kit) => {
+    kit_stats.wins.total[kit] = (kit_stats.wins.solo[kit] || 0) + (kit_stats.wins.teams[kit] || 0);
     kit_stats.k_d[kit] = getRatio(kit_stats.kills[kit], (kit_stats.games_played[kit] - ((kit_stats.wins.solo[kit] || 0) + (kit_stats.wins.teams[kit] || 0))));
     kit_stats.w_l[kit] = getRatio(((kit_stats.wins.solo[kit] || 0) + (kit_stats.wins.teams[kit] || 0)), (kit_stats.games_played[kit] - ((kit_stats.wins.solo[kit] || 0) + (kit_stats.wins.teams[kit] || 0))));
   });
