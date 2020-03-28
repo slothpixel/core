0be7b9560ab626425c3901b5ddd78d886b3d47f8
mattco98 <matthewcolsson@gmail.com>
2020-03-28 13:17:00 -0700

    Wrap graphql required functions with promisify once

--- routes/graphql.js
+++ routes/graphql.js
@@ -11,47 +11,55 @@ const redis = require('../store/redis');
 const getUUID = require('../store/getUUID');
 const { generateJob, getData, typeToStandardName } = require('../util/utility');
 
+const buildBansAsync = promisify(buildBans);
+const buildBoostersAsync = promisify(buildBoosters);
+const leaderboardsAsync = promisify(leaderboards);
+const getGuildFromPlayerAsync = promisify(getGuildFromPlayer);
+const getPlayerAsync = promisify(getPlayer);
+const redisGetAsync = promisify(redis.get);
+
+
 const graphql = graphqlExpress({
   schema: buildSchema(fs.readFileSync('./routes/spec.graphql', 'utf8')),
   rootValue: {
     bans() {
-      return promisify(buildBans)();
+      return buildBansAsync();
     },
 
     allBoosters() {
-      return promisify(buildBoosters)();
+      return buildBoostersAsync();
     },
 
     async boosters({ game }) {
-      const data = await promisify(buildBoosters)();
+      const data = await buildBoostersAsync();
       return data[game];
     },
 
     getLeaderboardTemplate({ template }) {
-      return promisify(leaderboards)(undefined, template);
+      return leaderboardsAsync(undefined, template);
     },
 
     guild({ playerName, populatePlayers }) {
-      return promisify(getGuildFromPlayer)(playerName, populatePlayers);
+      return getGuildFromPlayerAsync(playerName, populatePlayers);
     },
 
     leaderboards(params) {
-      return promisify(leaderboards)(params, null);
+      return leaderboardsAsync(params, null);
     },
 
     player({ playerName /* , fields */ }) {
       // TODO: Remove 'fields' param from the /players/{playerName} route.
       // If someone wants specific fields, they should use graphql.
-      return promisify(getPlayer)(playerName);
+      return getPlayerAsync(playerName);
     },
 
     async playerAchievements({ playerName }) {
-      const player = await promisify(getPlayer)(playerName);
+      const player = await getPlayerAsync(playerName);
       return player.achievements;
     },
 
     async playerQuests({ playerName }) {
-      const player = await promisify(getPlayer)(playerName);
+      const player = await getPlayerAsync(playerName);
       return player.quests;
     },
 
@@ -94,8 +102,7 @@ const graphql = graphqlExpress({
     },
 
     async skyblockItems() {
-      const items = await promisify(redis.get)('skyblock_items');
-      return JSON.parse(items);
+      return JSON.parse(await redisGetAsync('skyblock_items'));
     },
 
     skyblockProfiles(/* { playerName } */) {
