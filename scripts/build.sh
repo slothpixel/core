c568ef356327959d3a8c3a3a98346167204f1593
builder_247 <olaakkonen@gmail.com>
2020-04-20 14:07:13 +0300

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,4 @@
 # Exit on error
 set -e
 
-sudo docker run -d --privileged --name redis --net=host redis:5
-sudo docker run -d --privileged --name mongo --net=host mongo
-sudo docker build -t "slothpixel/core" . && sudo docker run --net=host -i slothpixel/core sh -c 'npm run test'
+sudo docker-compose up -d && sleep 30 && sudo docker exec -it slothpixel-core sh -c 'npm run test'
