af780756053527b1dff41668b6a7cf2bb07c4e9d
Richie Bendall <richiebendall@gmail.com>
2020-04-24 02:52:32 +1200

    Fix lint
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- store/cachedFunction.js
+++ store/cachedFunction.js
@@ -1,19 +1,19 @@
 const { read, write } = require('./cacheFunctions');
 
 module.exports = async (key, createCache, { cacheDuration = 60, shouldCache = true } = {}) => {
-	const cached = await read({ key });
-	if (cached) {
-		return cached;
-	}
+  const cached = await read({ key });
+  if (cached) {
+    return cached;
+  }
 
-	const result = await createCache();
+  const result = await createCache();
 
-	if (shouldCache) {
-		await write({
-			key,
-			duration: cacheDuration,
-		});
-	}
+  if (shouldCache) {
+    await write({
+      key,
+      duration: cacheDuration,
+    });
+  }
 
-	return result;
+  return result;
 };
