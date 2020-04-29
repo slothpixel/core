d0295d10bc2fa8173df5d2b7fe2ab881bbf2250a
Richie Bendall <richiebendall@gmail.com>
2020-04-30 01:52:04 +1200

    Fix `buildBazaar is not a function`
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- store/buildBazaar.js
+++ store/buildBazaar.js
@@ -3,7 +3,7 @@ const redis = require('./redis');
 const cachedFunction = require('./cachedFunction');
 const { getData, generateJob } = require('../util/utility');
 
-module.exports = cachedFunction('bazaar', async () => {
+module.exports = async () => cachedFunction('bazaar', async () => {
   const { products } = await getData(redis, generateJob('bazaar_products').url);
   return products;
 });
