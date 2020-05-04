fc4138b377f13edb23575f238d894349c008f234
Richie Bendall <richiebendall@gmail.com>
2020-05-05 02:21:13 +1200

    Log error message
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- store/buildPlayer.js
+++ store/buildPlayer.js
@@ -68,7 +68,7 @@ async function populatePlayers(players) {
       await pify(queries.cachePlayerProfile)(profile);
       return player;
     } catch (error) {
-      logger.error(error);
+      logger.error(error.message);
     }
   });
 }
