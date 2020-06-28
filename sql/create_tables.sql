b524f64fcf316d1aa33ccb8f08bcc8077e2e9768
builder_247 <olaakkonen@gmail.com>
2020-06-29 01:02:09 +0300

    Proposed auction schema

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,14 @@
--- todo
+CREATE TABLE IF NOT EXISTS auctions (
+    uuid uuid PRIMARY KEY,
+    start integer,
+    "end" integer,
+    tier varchar(255),
+    category varchar(255),
+    item jsonb[],
+    starting_bid integer,
+    highest_bid integer,
+    bids jsonb[],
+    highest_bid_amount integer
+);
+CREATE INDEX IF NOT EXISTS auctions_uuid_idx on auctions(uuid);
+CREATE INDEX IF NOT EXISTS item_id_idx on auctions USING GIN ((item -> 'attributes' -> 'id'));
