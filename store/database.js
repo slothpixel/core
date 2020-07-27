5f0e2b610ce2fb18f9c6ead21e5486d9ee72ae9e
builder_247 <olaakkonen@gmail.com>
2020-07-27 18:55:14 +0300

    Replace knex with slonik

--- store/database.js
+++ store/database.js
@@ -1,19 +1,9 @@
 /**
  * Interface to PostgreSQL client
  * */
-const pg = require('pg');
-const knex = require('knex');
-const { logger } = require('../util/utility');
+const { createPool } = require('slonik');
 const config = require('../config');
 
-// remember: all values returned from the server are either NULL or a string
-pg.types.setTypeParser(20, (value) => (value === null ? null : Number.parseInt(value, 10)));
-logger.info(`connecting ${config.POSTGRES_URL}`);
-const database = knex({
-  client: 'pg',
-  connection: config.POSTGRES_URL,
-});
-database.on('query-error', (error) => {
-  logger.error(error);
-});
-module.exports = database;
+const pool = createPool(config.POSTGRES_URL);
+
+module.exports = pool;
