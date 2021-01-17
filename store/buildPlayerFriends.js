d36ea6c640269a1a8f65725e623585ee76758320
Patrick Jones <pjones-uk@outlook.com>
2021-01-17 19:25:05 +0000

    fix lint

--- store/buildPlayerFriends.js
+++ store/buildPlayerFriends.js
@@ -4,12 +4,12 @@ const redis = require('./redis');
 const getUUID = require('./getUUID');
 
 module.exports = async (username) => cachedFunction(`friends:${username}`, async () => {
-    const uuid = await getUUID(username)
-    const data = await getData(redis, generateJob('friends', { id: uuid }));
-    const friends = data.records || [];
-    return friends.map(friend => ({
-        uuid: (friend.uuidSender !== uuid) ? friend.uuidSender : friend.uuidReceiver,
-        sent_by: friend.uuidSender,
-        started: friend.started
-    }))
-})
+  const uuid = await getUUID(username);
+  const data = await getData(redis, generateJob('friends', { id: uuid }));
+  const friends = data.records || [];
+  return friends.map((friend) => ({
+    uuid: (friend.uuidSender !== uuid) ? friend.uuidSender : friend.uuidReceiver,
+    sent_by: friend.uuidSender,
+    started: friend.started,
+  }));
+});
