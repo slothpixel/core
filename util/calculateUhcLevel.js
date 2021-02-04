a2b1b882bf80ca074f2d145d73210622007bb493
surprise <ethanmgentz@gmail.com>
2021-02-04 17:06:49 -0500

    Add basic uhc stats to gql and add level to players endpoint

---
+++ util/calculateUhcLevel.js
@@ -0,0 +1,17 @@
+/*
+* Functions for UHC exp and level conversions.
+ */
+
+function getLevelForScore(xp) {
+  const scores = [0, 10, 60, 210, 460, 960, 1710, 2710, 5210, 10210, 13210, 16210, 19210, 22210, 25210];
+  let level = 0;
+  for (const score of scores) {
+    if (xp >= score) level += 1;
+    else break;
+  }
+  return level;
+}
+
+module.exports = {
+  getLevelForScore: getLevelForScore,
+};
