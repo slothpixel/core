f4e0090c230541828131fabb4c4c39f96e73a5bb
Squagward <cam.n.walter@gmail.com>
2021-04-21 21:51:13 -0500

    added naming to party games and tnt tag
    I could do something nicer in the toMode function, but I think it's okay for now

--- processors/processGameCounts.js
+++ processors/processGameCounts.js
@@ -17,26 +17,34 @@ function processGameCounts(data) {
   };
   const { games: games_, playerCount } = data;
   for (const [rawGame, data_] of Object.entries(games_)) {
-    const cleanGame = toMode(rawGame.replace(/_LOBBY/, ''), modes) || rawGame;
+    const cleanGame = toMode(rawGame.replace(/_LOBBY/, ''), modes);
     if (data_.modes && Object.keys(data_.modes).length > 1) {
       const modes_ = {};
       for (const [rawMode, count] of Object.entries(data_.modes)) {
-        const cleanMode = toMode(rawMode, modes) || rawMode;
+        const cleanMode = toMode(rawMode, modes);
         modes_[rawMode] = {};
-        if (cleanMode !== rawMode) {
-          modes_[rawMode].name = cleanMode;
+        switch (rawMode) {
+          case "PARTY": 
+            modes_[rawMode].name = "Party Games";
+            break;
+          case "TNTAG":
+            modes_[rawMode].name = "TNT Tag";
+            break;
+          default:
+            modes_[rawMode].name = cleanMode;
+            break;
         }
         modes_[rawMode].players = count;
       }
       object.games[rawGame] = {};
-      if (cleanGame !== rawGame) {
+      if (cleanGame) {
         object.games[rawGame].name = cleanGame;
       }
       object.games[rawGame].players = data_.players;
       object.games[rawGame].modes = modes_;
     } else {
       object.games[rawGame] = {};
-      if (cleanGame !== rawGame) {
+      if (cleanGame) {
         object.games[rawGame].name = cleanGame;
       }
       object.games[rawGame].players = data_.players;
