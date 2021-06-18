0f0f4df99b15b18636d98e03935d2a63440e0092
Matthew <mattvine03@gmail.com>
2021-06-18 09:56:35 -0400

    Whoops, typo

--- processors/processSkyBlock.js
+++ processors/processSkyBlock.js
@@ -25,7 +25,7 @@ async function checkItems(members = {}) {
         if (![null, undefined].includes(item.attributes.modifier)
           || id.startsWith('MAP:')
           || item.name === '§fnull'
-          || item.name.endWith('✪')
+          || item.name.endsWith('✪')
           || item.name.startsWith('Thick ')
           || !/[!-~]/.test(item.name) || !/[!-~]/.test(item.type)
           || item.rarity_upgrades) return [];
