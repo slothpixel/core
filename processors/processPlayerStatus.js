43e24b36f3542f94a71a6dbfaf3a842e0274a15f
TheMysterys <42754166+TheMysterys@users.noreply.github.com>
2021-07-07 20:23:01 +1200

    Added new system for sorting through gamemodes in Hypixel Constants

--- processors/processPlayerStatus.js
+++ processors/processPlayerStatus.js
@@ -1,13 +1,28 @@
 const { game_types: gameTypes, modes, maps } = require('hypixelconstants');
 
+// Constant Values for maps and game types
 const gameTypes_ = new Map(gameTypes
   .map(({ type_name: typeName, clean_name: cleanName }) => [typeName, cleanName]));
-const modes_ = new Map(modes
-  .map(({ key, modes }) => [key, modes])
-  .filter(([_, modes]) => modes)
-  .map(([key, modes]) => [key, new Map(modes.map(({ key, name }) => [key, name]))]));
 const maps_ = new Map(Object.entries(maps)
   .map(([game, maps]) => [game, new Map(Object.entries(maps).map((([map, { name }]) => [map, name])))]));
+
+// Used for getting sub gamemodes
+function getMode(type, mode){
+	let gameObject = modes.find((modeObject) => modeObject.key === type);
+	if (!gameObject) return mode
+	let subGameObject = gameObject.modes.find(
+		(modeObject) => modeObject.key === "DREAMS" || modeObject.key === "lab"
+	);
+	return modes_ = gameObject.modes.find((modeObject) =>
+		modeObject.keys ? modeObject.keys.includes(mode) : modeObject.key == mode
+	)
+	? gameObject.modes.find((modeObject) =>
+		modeObject.keys ? modeObject.keys.includes(mode) : modeObject.key == mode
+	)
+	: subGameObject.modes.find((modeObject) =>
+		modeObject.keys ? modeObject.keys.includes(mode) : modeObject.key == mode
+	)
+}
 
 module.exports = ({
   online,
@@ -18,7 +33,7 @@ module.exports = ({
   online,
   game: {
     type: gameTypes_.get(type) || null,
-    mode: mode === 'LOBBY' ? 'Lobby' : (modes_.has(type) ? modes_.get(type).get(mode) || null : null),
+    mode: mode === 'LOBBY' ? 'Lobby' : (getMode(type, mode)? getMode(type, mode).name || getMode(type, mode) : null),
     map: maps_.has(type) ? maps_.get(type).get(map) || null : map || null,
   },
 });
