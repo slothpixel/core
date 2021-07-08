e85fd65294c3d399890f922a06dd972546e53596
TheMysterys <42754166+TheMysterys@users.noreply.github.com>
2021-07-08 12:16:21 +1200

    Reduced amount of Conditional operators used
    Trying to see if there is a way to not need any for the `return` part but at current stage they seem required

--- processors/processPlayerStatus.js
+++ processors/processPlayerStatus.js
@@ -13,14 +13,11 @@ function getMode(type, mode){
 	let subGameObject = gameObject.modes.find(
 		({key}) => key === "DREAMS" || key === "lab"
 	);
-	return modes_ = gameObject.modes.find((modeObject) =>
-		modeObject.keys ? modeObject.keys.includes(mode) : modeObject.key == mode
+	return modes_ = gameObject.modes.find(({key,keys}) =>
+		keys ? keys.includes(mode) || key == mode : key == mode
 	)
-	? gameObject.modes.find((modeObject) =>
-		modeObject.keys ? modeObject.keys.includes(mode) : modeObject.key == mode
-	)
-	: subGameObject.modes.find((modeObject) =>
-		modeObject.keys ? modeObject.keys.includes(mode) : modeObject.key == mode
+	|| subGameObject.modes.find(({key, keys}) =>
+		keys ? keys.includes(mode) || key == mode : key == mode
 	)
 }
 
