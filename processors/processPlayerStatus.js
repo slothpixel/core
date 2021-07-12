74cad1768351c11461723468f40bde19fbe381d7
TheMysterys <42754166+TheMysterys@users.noreply.github.com>
2021-07-12 13:12:42 +1200

    Fixed errors

--- processors/processPlayerStatus.js
+++ processors/processPlayerStatus.js
@@ -7,18 +7,17 @@ const maps_ = new Map(Object.entries(maps)
   .map(([game, maps]) => [game, new Map(Object.entries(maps).map((([map, { name }]) => [map, name])))]));
 
 // Used for getting sub gamemodes
-function getMode(type, mode){
-	let gameObject = modes.find((modeObject) => modeObject.key === type);
-	if (!gameObject) return mode
-	let subGameObject = gameObject.modes.find(
-		({key}) => key === "DREAMS" || key === "lab"
-	);
-	return modes_ = gameObject.modes.find(({key,keys}) =>
-		keys ? keys.includes(mode) || key == mode : key == mode
-	)
-	|| subGameObject.modes.find(({key, keys}) =>
-		keys ? keys.includes(mode) || key == mode : key == mode
-	)
+function getMode(type, mode) {
+  const gameObject = modes.find((modeObject) => modeObject.key === type);
+  if (!gameObject) return mode;
+  const subGameObject = gameObject.modes.find(
+    ({ key }) => key === 'DREAMS' || key === 'lab',
+  );
+  return gameObject.modes.find(({ key, keys }) => (
+    keys ? keys.includes(mode) || key === mode : key === mode
+  )) || subGameObject.modes.find(({ key, keys }) => (
+    keys ? keys.includes(mode) || key === mode : key === mode
+  ));
 }
 
 module.exports = ({
