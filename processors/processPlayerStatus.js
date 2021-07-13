cb68c2f4d18071e0bfd2b44703894c23e2187b04
TheMysterys <42754166+TheMysterys@users.noreply.github.com>
2021-07-14 09:13:33 +1200

    Adding changes

--- processors/processPlayerStatus.js
+++ processors/processPlayerStatus.js
@@ -6,15 +6,15 @@ const maps_ = new Map(Object.entries(maps)
   .map(([game, maps]) => [game, new Map(Object.entries(maps).map((([map, { name }]) => [map, name])))]));
 
 function getMode(type, mode){
-  let gameObject = modes.find(({key}) => key === type);
-  if (!gameObject) return mode
-  let subGameObject = gameObject.modes.find(
+  const game = modes.find(({key}) => key === type);
+  if (!game) return mode
+  const subGame = game.modes.find(
     ({key}) => key === "DREAMS" || key === "lab"
    );
-  return gameObject.modes.find(({key, keys}) =>
+  return game.modes.find(({key, keys}) =>
     (keys && keys.includes(mode)) || key == mode
   )
-  || (subGameObject && subGameObject.modes.find(({key, keys}) =>
+  || (subGame && subGame.modes.find(({key, keys}) =>
     (keys && keys.includes(mode)) || key == mode
   ))
 }
