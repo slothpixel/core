746c68017765e1c42a93282514b71cfa2986bcc4
TheMysterys <42754166+TheMysterys@users.noreply.github.com>
2021-07-13 19:11:31 +1200

    Fixed spacing and Useless conditional
    https://github.com/slothpixel/core/pull/668#issuecomment-878835932

--- processors/processPlayerStatus.js
+++ processors/processPlayerStatus.js
@@ -6,16 +6,16 @@ const maps_ = new Map(Object.entries(maps)
   .map(([game, maps]) => [game, new Map(Object.entries(maps).map((([map, { name }]) => [map, name])))]));
 
 function getMode(type, mode){
-	let gameObject = modes.find(({key}) => key === type);
-	if (!gameObject) return mode
-	let subGameObject = gameObject.modes.find(
-		({key}) => key === "DREAMS" || key === "lab"
-	);
-	return (gameObject && gameObject.modes.find(({key,keys}) =>
-  (keys && keys.includes(mode)) || key == mode
+  let gameObject = modes.find(({key}) => key === type);
+  if (!gameObject) return mode
+  let subGameObject = gameObject.modes.find(
+    ({key}) => key === "DREAMS" || key === "lab"
+   );
+  return (gameObject && gameObject.modes.find(({key,keys}) =>
+    (keys && keys.includes(mode)) || key == mode
 	))
 	|| (subGameObject && subGameObject.modes.find(({key, keys}) =>
-  (keys && keys.includes(mode)) || key == mode
+    (keys && keys.includes(mode)) || key == mode
 	))
 }
 
