30ef783706a58bfc5c194a4812b3d8bb0d9ac0ec
TheMysterys <42754166+TheMysterys@users.noreply.github.com>
2021-07-13 19:12:52 +1200

    Forgot to make changes for Useless conditional

--- processors/processPlayerStatus.js
+++ processors/processPlayerStatus.js
@@ -11,9 +11,9 @@ function getMode(type, mode){
   let subGameObject = gameObject.modes.find(
     ({key}) => key === "DREAMS" || key === "lab"
    );
-  return (gameObject && gameObject.modes.find(({key,keys}) =>
+  return gameObject.modes.find(({key,keys}) =>
     (keys && keys.includes(mode)) || key == mode
-	))
+  )
 	|| (subGameObject && subGameObject.modes.find(({key, keys}) =>
     (keys && keys.includes(mode)) || key == mode
 	))
