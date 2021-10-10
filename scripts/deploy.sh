126180c43b4a39f048d5f6a95aa3cf4033040054
builder_247 <14019974+builder-247@users.noreply.github.com>
2021-10-10 16:24:26 +0300

    removed legacy stuff (#705)

--- scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for instance in `gcloud compute instance-groups managed list-instances $1-group-1 --format "value(NAME)"` ;
-do
-  gcloud compute instance-groups managed recreate-instances $1-group-1 --instances $instance;
-  sleep ${2:-180};
-done
--- scripts/web.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl -sSL https://get.docker.com/ | sh
-sudo docker run -d --name=web --restart=always --net=host --log-opt max-size=1g -e FRONTEND_PORT=80 -e PROVIDER=gce -e ROLE=web slothpixel/core:latest sh -c "npm start"
-sudo docker start web
