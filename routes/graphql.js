20fa9b5cb4b36214c8aa58c179bd44d907f6ab36
Patrick Jones <pjones-uk@outlook.com>
2022-06-30 20:13:48 +0100

    fix lint

--- routes/graphql.js
+++ routes/graphql.js
@@ -64,8 +64,8 @@ class PlayersResolver {
 
   async achievements({ player_name }) {
     const { achievements } = await getPlayer(player_name);
-    achievements.games = Object.keys(achievements.games).map((key) => Object.assign({ name: key }, achievements.games[key]));
-    return achievements
+    achievements.games = Object.keys(achievements.games).map((key) => ({ name: key, ...achievements.games[key] }));
+    return achievements;
   }
 
   async quests({ player_name }) {
